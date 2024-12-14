1f641d1f895fb889ad2e0c4de21eea5eb7c956db
daotanhao1406 <daotanhao9h@gmail.com>
2024-12-14 21:29:54 +0700

    update: update icon

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,4 +1,4 @@
-import { MessageCircle, Linkedin, Mail, Github, Twitter, Facebook, Instagram, Video } from "lucide-react";
+import { MessageCircle, Linkedin, Mail, Github, Twitter, Facebook, Instagram } from "lucide-react";
 import { LucideIcon } from "lucide-react";
 
 export interface MenuItem {
